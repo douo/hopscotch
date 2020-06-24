c3bd2662820b3800be4bc87449b507f61137598a
tiou <dourokinga@gmail.com>
2020-06-24 16:14:34 +0800

    Initial Commit

---
+++ Demo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Demo
+//
+//  Created by Tiou Lims on 2020/6/24.
+//  Copyright © 2020 Tiou Lims. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
